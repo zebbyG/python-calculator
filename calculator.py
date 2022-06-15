9e8a9ece07bf52223eeeb5c0553858925dc86dd5
zebbyG <99161685+zebbyG@users.noreply.github.com>
2022-06-15 09:52:05 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-digit1 = int(input("Enter first digit: "))
+digit1 = float(input("Enter first digit: "))
 mathopp = input("Enter operator: ")
-digit2 = int(input("Enter last digit: "))
+digit2 = float(input("Enter last digit: "))
 if mathopp == '+':
     print(digit1 + digit2)
 
