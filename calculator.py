365becf6c281c76ac3636729ca79d3f9f0282d8e
zebbyG <zebbynjau@gmail.com>
2023-02-15 15:34:41 +0300

    Modify...add warnings

--- calculator.py
+++ calculator.py
@@ -1,26 +1,42 @@
-digit1 = float(input("Enter first digit: "))
-mathopp = input("Enter operator: ")
-digit2 = float(input("Enter last digit: "))
-if mathopp == '+':
-    print(digit1 + digit2, round(float, 2))
+while True:
+    try:
+        digit1 = float(input("Enter first digit: "))
+        break
+    except ValueError:
+        print("\033[1;31mPlease enter a valid number\033[0m")
+math_opp = input("Enter operator: ")
+while True:
+    try:
+        digit2 = float(input("Enter last digit: "))
+        break
+    except ValueError:
+        print("\033[1;31mPlease enter a valid number\033[0m")
 
-elif mathopp == '-':
-    print(digit1 - digit2, round(float, 2))
+if math_opp == "+":
+    result = digit1 + digit2
 
-elif mathopp == '%':
-    print(digit1 % digit2)
+elif math_opp == "-":
+    result = digit1 - digit2
 
-elif mathopp == '*':
-    print(digit1 * digit2)
+elif math_opp == "%":
+    result = digit1 % digit2
 
-elif mathopp == '/':
-    print(digit1 / digit2)
+elif math_opp == "*":
+    result = digit1 * digit2
 
-elif mathopp == '//':
-    print(digit1 // digit2)
+elif math_opp == "/":
+    result = digit1 / digit2
 
-elif mathopp == '**':
-    print(digit1 ** digit2)
+elif math_opp == "//":
+    result = digit1 // digit2
+
+elif math_opp == "**":
+    result = digit1 ** digit2
 
 else:
-    print("404 operator not found. Try again with a different opperator :) ")
+    print("404 operator" + " " + f"\033[1;31m{math_opp}\033[0m not found." + "Try again with a valid operator :)")
+    result = None
+
+if result is not None:
+    print(f"\033[32mAnswer = {round(result, 3)}\033[0m")
+
